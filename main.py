8727c8d5dc30acde1a6dd8f9e071ea57ccc9e178
wrednaya33 <novikova@yandexlyceum.ru>
2020-04-19 18:30:22 +0300

    баги в облаке3

--- main.py
+++ main.py
@@ -335,7 +335,8 @@ class LoginForm(FlaskForm):
 def main():
     db_session.global_init("db/CrossStitch.sqlite")
     # push.push_data()
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
